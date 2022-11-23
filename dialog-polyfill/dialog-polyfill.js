543af08b7a3fc414da9a5060763d24081e9ba6fb
Michael Nelson <michael.nelson@gov.bc.ca>
2022-11-23 09:12:27 -0800

    Add comment to copied script

--- dialog-polyfill/dialog-polyfill.js
+++ dialog-polyfill/dialog-polyfill.js
@@ -1,3 +1,4 @@
+// Copied from smk-tlink app
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
