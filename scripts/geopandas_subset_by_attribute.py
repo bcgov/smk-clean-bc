2015144a20eeec4ce676435dbb9e032a5392c268
Michael Nelson <michael.nelson@gov.bc.ca>
2022-12-16 11:23:39 -0800

    Replaced OS-specific delimiters in paths

--- scripts/geopandas_subset_by_attribute.py
+++ scripts/geopandas_subset_by_attribute.py
@@ -6,7 +6,7 @@ different values of a given attribute
 import os
 import geopandas
 
-geojson_in = r"../layers/evcs-stalls.geojson"
+geojson_in = os.path.join("..", "layers", "evcs-stalls.geojson")
 data = geopandas.read_file(geojson_in)
 outdir = os.path.dirname(geojson_in)
 
@@ -17,4 +17,4 @@ col = "Stall_Type"
 # %%
 for val in getattr(data, col).unique():
     out = data[getattr(data, col)==val]
-    out.to_file(outdir+r"/"+geojson_base+"_"+val+".geojson", driver="GeoJSON")
+    out.to_file(os.path.join(outdir, geojson_base+"_"+val+".geojson"), driver="GeoJSON")
