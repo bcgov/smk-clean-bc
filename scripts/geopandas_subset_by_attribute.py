30d288ff16e14ecd1ad4392edb15d2a7ec782134
Michael Nelson <michael.nelson@gov.bc.ca>
2022-12-16 11:10:20 -0800

    POC of splitting geoJson layer by attribute

---
+++ scripts/geopandas_subset_by_attribute.py
@@ -0,0 +1,20 @@
+'''
+take GeoJSON data and create subsets for 
+different values of a given attribute
+'''
+#%%
+import os
+import geopandas
+
+geojson_in = r"../layers/evcs-stalls.geojson"
+data = geopandas.read_file(geojson_in)
+outdir = os.path.dirname(geojson_in)
+
+geojson_base = os.path.splitext(os.path.basename(geojson_in))[0]
+
+# choose the attribute to subset by
+col = "Stall_Type"
+# %%
+for val in getattr(data, col).unique():
+    out = data[getattr(data, col)==val]
+    out.to_file(outdir+r"/"+geojson_base+"_"+val+".geojson", driver="GeoJSON")
